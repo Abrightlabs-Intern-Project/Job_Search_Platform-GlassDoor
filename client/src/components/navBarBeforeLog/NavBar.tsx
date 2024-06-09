11a93a2bdb2c40faf8ded931e33cf09f7a9faac4
saran <saransathi1010@gmail.com>
2024-06-10 01:19:22 +0530

    adding navigation bar to client

---
+++ client/src/components/navBarBeforeLog/NavBar.tsx
@@ -0,0 +1,55 @@
+// src/components/NavBar.tsx
+import React, { useState } from 'react';
+import './css/NavBar.css';
+import Logo from './images/glassdoor.png';
+import siginicon from './images/icons8-login-48.png'
+
+const NavBar: React.FC = () => {
+    const [hoveredItem, setHoveredItem] = useState<string | null>(null);
+
+    const handleMouseEnter = (item: string) => {
+        setHoveredItem(item);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredItem(null);
+    };
+    return (
+        <>
+
+<nav className="navbar">
+            <div className="navbar-left">
+                <img src={Logo} alt="Left Icon" className="navbar-left-icon" />
+            </div>
+            <div className="navbar-center">
+                <ul className="nav-links">
+                    {['Community', 'Services', 'Jobs', 'Companies'].map(item => (
+                        <li 
+                            key={item} 
+                            onMouseEnter={() => handleMouseEnter(item)} 
+                            onMouseLeave={handleMouseLeave}
+                        >
+                            {item}
+                            {hoveredItem === item && (
+                                <div className="hover-content">
+                                    {`Content for ${item}`}
+                                </div>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="navbar-right">
+                <button className="signin-button">
+                    <img src={siginicon} alt="Sign In" className="signin-icon" />
+                    Sign In
+                </button>
+            </div>
+        </nav>        
+
+        </>
+        
+    );
+}
+
+export default NavBar;
