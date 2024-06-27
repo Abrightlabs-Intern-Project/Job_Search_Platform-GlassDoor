c3b184ff2ad6aff76427ec2732c118112e1a4fee
saran <saransathi1010@gmail.com>
2024-06-27 15:18:56 +0530

    checking

--- api/src/main.ts
+++ api/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document)
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://main.daq8kzkn55nlw.amplifyapp.com','https://glassdoor.koyeb.app'],
     credentials: true,
   });
     await app.listen(port);
