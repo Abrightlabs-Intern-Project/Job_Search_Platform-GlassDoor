3b2777928751a4358c135cc31dcd306361bfcaa8
saran <saransathi1010@gmail.com>
2024-07-05 13:01:36 +0530

    'check'

--- client/src/components/Signin/firbase.ts
+++ client/src/components/Signin/firbase.ts
@@ -1,24 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCE9ORi0mOPP8UF9VlkV9gncSnxLkC-DEU",
-  authDomain: "jobsearch-a18ed.firebaseapp.com",
-  projectId: "jobsearch-a18ed",
-  storageBucket: "jobsearch-a18ed.appspot.com",
-  messagingSenderId: "114821173703",
-  appId: "1:114821173703:web:02ca9b321bf125f0e209e2",
-  measurementId: "G-KQGL4MJDER"
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth=getAuth();
-export const db=getFirestore(app);
+export const auth = getAuth();
+export const db = getFirestore(app);
 export default app;
